de4a3e8d9de62c6d85b46d3a05b994b171ab5d69
izzqz <v@izzqz.me>
2025-04-05 22:45:08 +0300

    Fall speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-@export var gravity: float = 2000    # Adjust based on desired fall speed
+@export var gravity: float = 500    # Adjust based on desired fall speed
 @export var move_speed: float = 200   # Horizontal movement speed
-@export var MAX_FALL_SPEED: float = 2000  # Maximum falling speed
+@export var MAX_FALL_SPEED: float = 600  # Maximum falling speed
 @export var direction_change_speed: float = 10.0  # How quickly direction changes (higher = faster)
 
 @export var MAX_CHANGE_SPEED: float = 1800 # Maximum speed after direction change
