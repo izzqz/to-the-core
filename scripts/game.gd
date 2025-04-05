8ee04d94b5eebbf27c17ca40b661961f0e1b353d
izzqz <v@izzqz.me>
2025-04-05 18:02:22 +0300

    hihihiha

---
+++ scripts/game.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var start_position = $Player.position
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("debug_restart"):
+		$Player.position = start_position
+		$Player.is_alive = false
+		#$Player.restart()
+
+	if event.is_action_pressed("debug_stop"):
+		$Player.is_alive = false
+		
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-@export var gravity: float = 400     # Adjust based on desired fall speed
-@export var move_speed: float = 400   # Horizontal movement speed
-@export var MAX_FALL_SPEED: float = 600  # Maximum falling speed
+@export var gravity: float = 200    # Adjust based on desired fall speed
+@export var move_speed: float = 200   # Horizontal movement speed
+@export var MAX_FALL_SPEED: float = 200  # Maximum falling speed
 @export var direction_change_speed: float = 10.0  # How quickly direction changes (higher = faster)
+
 @export var MAX_CHANGE_SPEED: float = 1800 # Maximum speed after direction change
-@export var MIN_CHANGE_SPEED: float = 500   # Minimum speed right after direction change
+@export var MIN_CHANGE_SPEED: float = 200   # Minimum speed right after direction change
+
 @export var speed_accel_rate: float = 1.5   # How quickly speed increases over time
 @export var time_to_max_speed: float = 0.5  # Time in seconds to reach max speed
 
@@ -15,8 +17,11 @@ var current_speed: float = move_speed  # Current movement speed
 var direction_timer: float = 0.0     # Timer for tracking how long we've moved in same direction
 
 @onready var colision_shape: CollisionShape2D = $CollisionShape2D
+@onready var is_alive = true
 
 func _physics_process(delta: float) -> void:
+	if not is_alive:
+		return
 	# Apply gravity to vertical velocity with MAX_FALL_SPEED limit
 	velocity.y += gravity * delta
 	velocity.y = min(velocity.y, MAX_FALL_SPEED)
@@ -49,18 +54,34 @@ func _physics_process(delta: float) -> void:
 	
 	# Rotate sprite based on interpolated direction
 	$AnimatedSprite2D.rotation_degrees = current_direction * -20
+	Global.add_score(1)
+	
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("flap"):
 		target_direction *= -1
 		direction_timer = 0.0
 		current_speed = MIN_CHANGE_SPEED
+		if not is_alive:
+			restart()
+
+func restart() -> void:
+	is_alive = true
+	Global.reset_score()
+	$AnimationPlayer.play("RESET")
+	$AnimatedSprite2D.play("default")
 
 func game_over() -> void:
+	is_alive = false
+	play_dead()
 	print("Game Over!")
-
 
 func _on_colision_detector_area_entered(area: Area2D) -> void:
 	print(area)
 	if area.is_in_group("obstacle"):
 		game_over()
+
+func play_dead() -> void:
+	$AnimatedSprite2D.stop()
+	$AnimationPlayer.play("death")
+	
